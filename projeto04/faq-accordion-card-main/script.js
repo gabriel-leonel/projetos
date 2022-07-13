3b8848a1036c94aa64f60246649f00737fcb38fd
Gabriel Leonel <gabriel.leonel2016@gmail.com>
2022-07-13 19:43:10 -0300

    projeto4

---
+++ projeto04/faq-accordion-card-main/script.js
@@ -0,0 +1,48 @@
+function abrirResposta0() {
+    let resposta0 = document.querySelector('p#resposta0') //primeiro p
+    if (resposta0.style.display == 'none') {
+        resposta0.style.display = 'inline'
+    }
+    else {
+        resposta0.style.display = 'none'
+    }
+}
+function abrirResposta1() {
+    let resposta1 = document.querySelector('p#resposta1')
+    if (resposta1.style.display == 'none') {
+        resposta1.style.display = 'inline'
+    }
+    else {
+        resposta1.style.display = 'none'
+    }
+}
+function abrirResposta2() {
+    let resposta2 = document.querySelector('p#resposta2')
+    if (resposta2.style.display == 'none') {
+        resposta2.style.display = 'inline'
+    }
+    else {
+        resposta2.style.display = 'none'
+    }
+}
+function abrirResposta3() {
+    let resposta3 = document.querySelector('p#resposta3')
+    if (resposta3.style.display == 'none') {
+        resposta3.style.display = 'inline'
+    }
+    else {
+        resposta3.style.display = 'none'
+    }
+}
+function abrirResposta4() {
+    let resposta4 = document.querySelector('p#resposta4')
+    if (resposta4.style.display == 'none') {
+        resposta4.style.display = 'inline'
+    }
+    else {
+        resposta4.style.display = 'none'
+    }
+}
+/*
+    
+*/
