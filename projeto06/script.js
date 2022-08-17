e4b2b4e5738a7366d522aef8d0d46eb81449bb05
Gabriel Leonel <gabriel.leonel2016@gmail.com>
2022-08-17 19:15:23 -0300

    melhora de desempenho

--- projeto06/script.js
+++ projeto06/script.js
@@ -1,14 +1,17 @@
 function aleatorio(nmin,nmax){ //função para gerar um número de 0 a 360
     nmin = Math.ceil(nmin)
     nmax = Math.floor(nmax)
-    n = Math.floor(Math.random() * (nmax-nmin)+nmin);
-    
+    n = Math.floor(Math.random()*(nmax-nmin)+nmin)
+    n2 = Math.floor(Math.random()*(100 - nmin)+nmin)
+    n3 = Math.floor(Math.random()*(100-nmin)+nmin)
     console.log(n)
-    return n
+    return n,n2,n3
 }
 
 function mudar() {
     aleatorio(0, 360)
     container = document.querySelector('div.container') //recebe o fundo como variavel
-    container.style.backgroundColor = `hsl(${n}, 50%, 50%)` //troca a cor de fundo
+    span = document.querySelector('span#span') //recebe o span
+    container.style.backgroundColor = `hsl(${n},${n2}%,${n3}%)` //troca a cor de fundo
+    span.innerHTML = container.style.backgroundColor
 }
