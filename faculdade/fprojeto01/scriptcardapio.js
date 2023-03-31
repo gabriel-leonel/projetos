a200472f30d5905f7ffdee9cfbaaf3bc25c40699
Gabriel Leonel <gabriel.leonel2016@gmail.com>
2023-03-30 21:38:10 -0300

    nova pasta para projetos da faculdade

---
+++ faculdade/fprojeto01/scriptcardapio.js
@@ -0,0 +1,34 @@
+var prato01 = document.querySelector('div#prato01')
+var prato02 = document.querySelector('div#prato02')
+var prato03 = document.querySelector('div#prato03')
+
+function next1() { 
+    prato01.style.display = "none"
+    prato02.style.display = "inline"
+    document.body.style.backgroundImage =  "url(/imgs/lasanha-grande.jpg)"
+}
+function next2() { 
+    prato02.style.display = "none"
+    prato03.style.display = "inline"
+    document.body.style.backgroundImage =  "url(/imgs/churros-grande.jpg)"
+}
+function next3() { 
+    prato03.style.display = "none"
+    prato01.style.display = "inline"
+    document.body.style.backgroundImage =  "url(/imgs/lula-grande.jpg)"
+}
+function previous1() { 
+    prato01.style.display = "none"
+    prato03.style.display = "inline"
+    document.body.style.backgroundImage =  "url(/imgs/churros-grande.jpg)"
+}
+function previous2() { 
+    prato02.style.display = "none"
+    prato01.style.display = "inline"
+    document.body.style.backgroundImage =  "url(/imgs/lula-grande.jpg)"
+}
+function previous3() { 
+    prato03.style.display = "none"
+    prato02.style.display = "inline"
+    document.body.style.backgroundImage =  "url(/imgs/lasanha-grande.jpg)"
+}
